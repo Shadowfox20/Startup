ea0b7167befbd0f9eac09843e6695b51b3a0495c
Robert Thompson <robertthompsoncalifornia@gmail.com>
2025-10-08 17:04:53 -0600

    fixed structure

--- src/app.jsx
+++ src/app.jsx
@@ -11,7 +11,7 @@ import { View } from './view/view';
 export default function App() {
  return (
   <BrowserRouter>
-  <body>
+  <div className="app-body">
    <header>
     <h1>Game Shelf</h1>
      <nav>
@@ -54,7 +54,7 @@ export default function App() {
      <a href="https://github.com/Shadowfox20/Startup"> GitHub </a>
     </p>
    </footer>
-  </body>
+  </div>
   </BrowserRouter>
  );
 }
